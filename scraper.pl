22570ac33063412cb2777b756ba94f1dec5a61be
Henare Degan <henare.degan@gmail.com>
2015-05-04 18:58:34 +1000

    Uncomment scraper so it runs

--- scraper.pl
+++ scraper.pl
@@ -1,33 +1,33 @@
 # This is a template for a Perl scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# use LWP::Simple;
-# use HTML::TreeBuilder;
-# use Database::DumpTruck;
+use LWP::Simple;
+use HTML::TreeBuilder;
+use Database::DumpTruck;
 
-# use strict;
-# use warnings;
+use strict;
+use warnings;
 
-# # Turn off output buffering
-# $| = 1;
+# Turn off output buffering
+$| = 1;
 
-# # Read out and parse a web page
-# my $tb = HTML::TreeBuilder->new_from_content(get('http://example.com/'));
+# Read out and parse a web page
+my $tb = HTML::TreeBuilder->new_from_content(get('http://example.com/'));
 
-# # Look for <tr>s of <table id="hello">
-# my @rows = $tb->look_down(
-#     _tag => 'tr',
-#     sub { shift->parent->attr('id') eq 'hello' }
-# );
+# Look for <tr>s of <table id="hello">
+my @rows = $tb->look_down(
+    _tag => 'tr',
+    sub { shift->parent->attr('id') eq 'hello' }
+);
 
-# # Open a database handle
-# my $dt = Database::DumpTruck->new({dbname => 'data.sqlite', table => 'data'});
-#
-# # Insert content of <td id="name"> and <td id="age"> into the database
-# $dt->insert([map {{
-#     Name => $_->look_down(_tag => 'td', id => 'name')->content,
-#     Age => $_->look_down(_tag => 'td', id => 'age')->content,
-# }} @rows]);
+# Open a database handle
+my $dt = Database::DumpTruck->new({dbname => 'data.sqlite', table => 'data'});
+
+# Insert content of <td id="name"> and <td id="age"> into the database
+$dt->insert([map {{
+    Name => $_->look_down(_tag => 'td', id => 'name')->content,
+    Age => $_->look_down(_tag => 'td', id => 'age')->content,
+}} @rows]);
 
 # You don't have to do things with the HTML::TreeBuilder and Database::DumpTruck
 # libraries. You can use whatever libraries are installed on morph.io for Perl
